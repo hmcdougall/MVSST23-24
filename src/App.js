130c06b75b1b95909a2b204024add4acd1d09704
hmcdougall <mcdoughn@miamioh.edu>
2023-08-15 11:54:09 -0400

    party mode

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import './App.css';
 import React, { useState } from 'react';
+import cal from './AllTeamCalJuly23.pdf'
+import Confetti from 'react-confetti'
 
 const NAMES = [
   'Annie',
@@ -32,13 +34,40 @@ function getName() {
 
 function App() {
   const [currentName, setCurrentName] = useState();
+  const [party, setParty] = useState(false)
 
   return (
     <div className="App">
       <header className="App-header">
-        <button type="button" onClick={() => setCurrentName(getName())}>Random Name</button>
-        <h3>{currentName}</h3>
+        <h1>MUSVSST 23-24</h1>
       </header>
+
+      <div className='section'>
+        <div className={'root' + (party ? ' party' : '')}>
+          <Confetti
+            // style={{ pointerEvents: 'none' }}
+            numberOfPieces={party ? 500 : 0}
+            recycle={false}
+            onConfettiComplete={confetti => {
+              setParty(false)
+              confetti.reset()
+            }}
+          />
+          <div>
+            <h3>Links</h3>
+            <a href={cal} target="_blank">Calender</a>
+            <br />
+            <a href='https://drive.google.com/drive/folders/1297K830XV66KvQmHEVpN5XNoz6Kj5PCL' target="_blank">Video Folder</a>
+          </div>
+
+          <br />
+          <button type="button" onClick={() => {
+            setCurrentName(getName());
+            setParty(!party);
+            }}>Random Name</button>
+          <h3>{currentName}</h3>
+        </div>
+      </div>
     </div>
   );
 }
